5942a7bcad9dfa5fdae5f54a617c715d4d10f1a5
Big_Earl <477117@chrome.escg.ac.uk>
2020-10-23 13:53:42 +0100

    First commit, ignore file and little level design

---
+++ Assets/Look.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    public float sensitivity = 100f;
+    public float xRotation = 0f;
+    public float yRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
