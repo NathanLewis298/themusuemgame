2205f1322253e9b624ed73ae2274aed94f9d94d4
Nathan <477117@chrome.escg.ac.uk>
2020-11-04 16:11:06 +0000

    Nathan's commit! (Vr controls, head movement and player movement

---
+++ Assets/Handpresence.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+public class Handpresence : MonoBehaviour
+{
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+    private GameObject spawnedModelPrefab;
+    private InputDevice targetDevice;
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+           Debug.Log(item.name + item.characteristics);
+        }
+    
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+
+
+
+            spawnedModelPrefab = Instantiate(handModelPrefab, transform);
+        }
+    
+    
+    
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
